e2d1bd40065eb3eafffc71b73c56fb00785a8075
Robert Lindohf <affe@affe-Latitude-D830.(none)>
2012-11-08 19:01:10 +0100

    Added template for binary encryption/decryption

---
+++ scripts/bin_data_template.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+class Data:
+    def __init__():
+        pass
+    def get_file_data():
+        pass
+    def get_stream_data(stream):
+        pass
+    def append_char():
+        pass
+
+class Encryption_template:
+    def __init__():
+        pass
+    def set_data_generator(gen):
+        pass
+    def set_key(key):
+        pass
+    def encrypt():
+        pass
+
+class Decryption_template:
+    def __init__():
+        pass
+    def set_data_generator(gen):
+        pass
+    def set_key(key):
+        pass
+    def decrypt():
+        pass
+
